77d9aa8ce05dfe3e903390f63e45a72d2e2dec94
deepanshu verma <deepanshuverma186@gmail.com>
2024-12-07 12:45:24 -0800

    custom pop & get

--- custom.js
+++ custom.js
@@ -13,12 +13,19 @@ class MyArray {
     get(value){
       return this.data[value] 
     }
+    pop(){
+       const lastIndex = this.length - 1
+       const item = this.data[lastIndex]
+       delete this.data[lastIndex]
+       this.length --
+       return item
+    }
 }
 
 
 const myNewArray = new MyArray()
 myNewArray.push('deepanshu')
 myNewArray.push('shivam')
-console.log(myNewArray.get(1))
-
+myNewArray.pop()
+console.log(myNewArray)
 
