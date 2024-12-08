4ac61bc888e26de96e67b69f3231194b3bcbebe0
deepanshu verma <deepanshuverma186@gmail.com>
2024-12-07 23:31:57 -0800

    shift method

--- custom.js
+++ custom.js
@@ -20,12 +20,22 @@ class MyArray {
        this.length --
        return item
     }
+    shift(){
+     const firstItem = this.data[0]
+     for(let i=0; i<this.length; i++){
+         this.data[i] = this.data[i+1]
+     }
+     delete this.data[this.length-1]
+     this.length --
+     return firstItem
+    }
 }
 
 
 const myNewArray = new MyArray()
 myNewArray.push('deepanshu')
 myNewArray.push('shivam')
-myNewArray.pop()
+myNewArray.push('insan')
+myNewArray.shift()
 console.log(myNewArray)
 
