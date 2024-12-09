cb336445cc529ba375faa629a33936403d69263f
deepanshu verma <deepanshuverma186@gmail.com>
2024-12-09 01:55:53 -0800

    reverse string & palindrome checker

--- methods.js
+++ methods.js
@@ -1,16 +1,39 @@
-function reverseArray(arr) {
-    let length = arr.length;
+// function reverseArray(arr) {
+//     let length = arr.length;
 
-    for (let i = 0; i < Math.floor(length / 2); i++) {
-        // Swap the elements
-        let temp = arr[i];
-        arr[i] = arr[length - 1 - i];
-        arr[length - 1 - i] = temp;
-    }
+//     for (let i = 0; i < Math.floor(length / 2); i++) {
+//         let temp = arr[i];
+//         arr[i] = arr[length - 1 - i];
+//         arr[length - 1 - i] = temp;
+//     }
 
-    return arr;
+//     return arr;
+// }
+
+// // Test the function
+// let numbers = [1, 2, 3, 4, 5];
+// console.log(reverseArray(numbers)); // Output: [5, 4, 3, 2, 1]
+
+
+
+
+function ReverseString (data){
+   const newData = data.split("").reverse().join('')
+   console.log(newData)
 }
 
-// Test the function
-let numbers = [1, 2, 3, 4, 5];
-console.log(reverseArray(numbers)); // Output: [5, 4, 3, 2, 1]
+ReverseString('sos')
+
+
+function isPalindrome(name){
+   const newData = name.split("").reverse().join('')
+   if(name === newData){
+    console.log('Its Palindrome')
+   }else{
+    console.log('Its not palindrome')
+   }
+}
+
+
+isPalindrome('sos')
+
