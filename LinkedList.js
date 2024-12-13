76e91c66005cbef50c5652bae2436ef5954f7262
deepanshu verma <deepanshuverma186@gmail.com>
2024-12-13 00:57:01 -0800

    linked list push method

--- LinkedList.js
+++ LinkedList.js
@@ -15,10 +15,20 @@ class LinkedList{
      this.tail = this.head
      this.length = 1
     } 
+    push(value){
+      let newNode = new node(value)
+      this.tail.next = newNode
+      this.tail = newNode
+      this.length ++;
+    }
 }
 
 const linkedlist = new LinkedList(1)
+linkedlist.push(10)
 console.log(linkedlist)
+
+
+
 
 
 
