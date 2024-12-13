0e8166d2fd43499697ba017408935cc2cbd8ff5b
deepanshu verma <deepanshuverma186@gmail.com>
2024-12-13 01:42:48 -0800

    linked list pop method

--- LinkedList.js
+++ LinkedList.js
@@ -17,14 +17,44 @@ class LinkedList{
     } 
     push(value){
       let newNode = new node(value)
+      if(!this.head){
+        this.head = newNode
+        this.tail = newNode
+      }
       this.tail.next = newNode
       this.tail = newNode
       this.length ++;
     }
+    pop(){
+        if(!this.head){
+            return undefined
+        }
+
+        let temp = this.head
+        let prev = this.head
+
+        while(temp.next){
+            prev = temp
+            temp = prev.next     
+        }
+        this.tail = prev
+        this.tail.next = null
+        this.length--;
+         
+        if(this.length === 0){
+            this.head = null
+            this.tail = null   
+        }
+
+        return temp
+
+    }
+   
 }
 
 const linkedlist = new LinkedList(1)
 linkedlist.push(10)
+linkedlist.pop()
 console.log(linkedlist)
 
 
