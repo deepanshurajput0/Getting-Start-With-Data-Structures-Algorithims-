5055f1ee5730946cc406a4efc1035cc7f027303c
deepanshu verma <deepanshuverma186@gmail.com>
2024-12-23 03:20:17 -0800

    Pushing Elements in Stack

--- Stacks.js
+++ Stacks.js
@@ -12,7 +12,20 @@ class Stack {
      this.first = newNode
      this.length = 1
     }
+    push(value){
+     let newNode = new Node(value)
+     if(this.length ===0){
+        this.first = newNode
+     }else{
+     newNode.next = newNode
+     this.first = newNode
+     this.length++
+     return this
+     }
+    }
 }
 
 let myStack = new Stack(0)
+myStack.push(1)
+myStack.push(2)
 console.log(myStack)
