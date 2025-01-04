580d39a4e5b292ba8aecdc7052512c391f82913b
deepanshu verma <deepanshuverma186@gmail.com>
2025-01-03 16:10:46 -0800

    Add up the Numbers from a Single Number

--- exercises/day1.js
+++ exercises/day1.js
@@ -9,3 +9,44 @@ function convertIntoSec(num){
 
 convertIntoSec(2)
 
+// Return the Next Number from the Integer Passed
+
+function Increment(num){
+    let result = num+1
+    console.log(result)
+}
+
+Increment(10)
+
+
+// Area of Triangle 
+
+function Area(b,h){
+   let result = 1/2 * b * h 
+   console.log(result)
+}
+
+Area(10,15)
+
+
+// Convert Age Into Days 
+
+function ageIntoDays(num){
+   let result = num * 365
+   console.log(result)
+}
+
+ageIntoDays(20)
+
+
+// Add up the Numbers from a Single Number
+
+function totalSum(n){
+    let sum = 0
+  for(let i=1; i<=n; i++){
+    sum = sum + i
+  }
+  console.log(sum)
+}
+
+totalSum(4)
