49adf45641753efca2d533c7fd85e8a41d4b0a0b
deepanshu verma <deepanshuverma186@gmail.com>
2025-01-13 16:52:57 -0800

    Implement function that returns a new function to sum base number with a given argument

--- exercises/day4.js
+++ exercises/day4.js
@@ -14,3 +14,70 @@ function noOfDigits(number){
 
 let result = noOfDigits(0)
 console.log(result)
+
+
+
+// Which Generation Are You?
+
+// Try finding your ancestors and offspring with code.
+
+// Create a function that takes a number x and a character y ("m" for male, "f" for female), and returns the name of an ancestor (m/f) or descendant (m/f).
+
+//     If the number is negative, return the related ancestor.
+//     If positive, return the related descendant.
+//     You are generation 0. In the case of 0 (male or female), return "me!".
+
+
+
+function GenerationFinder(x,y){
+   if(x===0){
+    return 'me'
+   }
+   if(x<0){
+     if(y==='m'){
+       if(x===-1) return 'father'
+       if(x===-2) return 'grandfather'
+       if(x===-3) return 'greatgrandfather'
+     }
+     if(y==='f'){
+        if(x===-1) return 'mother'
+        if(x===-2) return 'grandmother'
+        if(x===-3) return 'greatgrandmother'
+     }
+   }
+   if(x>0){
+    if(y==='m'){
+      if(x===1) return 'son'
+      if(x===2) return 'grandSon'
+      if(x===3) return 'greatgrandSon'
+    }
+    if(y==='f'){
+       if(x===1) return 'daughter'
+       if(x===2) return 'grandDaughter'
+       if(x===3) return 'greatgrandDaughter'
+    }
+  }
+
+
+}
+
+let result2 = GenerationFinder(-1,'m')
+console.log(result2)
+
+
+
+// Create a function that takes a "base number" as an argument. This function should return another function which takes a new argument, and returns the sum of the "base number" and the new argument.
+
+
+
+function functionMaker(baseNumber){
+      return function plus_baseNumber(number){
+         return number + baseNumber
+    }
+   
+}
+
+const addFive =  functionMaker(5)
+let result3 = addFive(5)
+console.log(result3)
+
