eb83a9f3a4a90a7f22b3463a523b4051811cc88e
deepanshu verma <deepanshuverma186@gmail.com>
2025-01-16 15:58:26 -0800

    check numbers in arrays of string

--- exercises/day5.js
+++ exercises/day5.js
@@ -19,3 +19,43 @@ function EvenOddChecker(number){
 
 const result = EvenOddChecker(2426)
 console.log(result)
+
+
+// Write a function that takes a string input and returns the string in a reversed case and order.
+
+
+function ReverseString(data){
+    return data.split('').reverse().join('')
+}
+let result2 = ReverseString('chirag')
+console.log(result2)
+
+
+///Create a function that takes an array of strings and returns an array with only the strings that have numbers in them. If there are no strings containing numbers, return an empty array.
+
+// numInStr(["1a", "a", "2b", "b"]) ➞ ["1a", "2b"]
+
+function numInStr(arr) {
+    const containsNumber = (str) => {
+      for (let char of str) {
+        if (!isNaN(char) && char !== ' ') { 
+          return true;
+        }
+      }
+      return false;
+    };
+  
+    return arr.filter(containsNumber);
+  }
+
+  const result3 = numInStr(["1a", "a", "2b", "b",'556b']);
+  console.log(result3);
+  
+
+  const nums =['2','3','2a','a']
+ 
+  for(let i=0; i<nums.length; i++){
+     if(!isNaN(nums)){
+        console.log(nums[i])
+     }
+  }
