2370a0253fbc9f5bb091015a6a1320e105a34170
deepanshu verma <deepanshuverma186@gmail.com>
2025-01-19 10:16:19 -0800

    Left Triangular star pattern

---
+++ exercises/day7.js
@@ -0,0 +1,22 @@
+/// printing patterns 
+
+// for(let i=1; i<=5; i++){
+//    let start = ' '
+//    for(let j=1; j<=i; j++){
+//       start = start + '*'
+//    }
+//    console.log(start)
+// }
+
+
+let num=5
+let pattern = ""
+for(let i=1; i<=num; i++){
+   pattern = ""
+    for(let j=0; j<=(num-i); j++){
+      pattern = pattern + "*"
+    }
+    console.log(pattern)
+}
+
+
