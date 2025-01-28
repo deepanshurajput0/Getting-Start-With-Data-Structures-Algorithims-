cf86c5dff8eb9567add5b43eacc2b4b14917f56c
deepanshu verma <deepanshuverma186@gmail.com>
2025-01-28 13:11:18 -0800

    sort array in descending order

--- exercises/day9.js
+++ exercises/day9.js
@@ -13,3 +13,19 @@ for(let i=0; i<myData.length; i++){
     }
 }
 console.log(myData)
+
+
+// Array Sort in Descending Order 
+
+const myData2 = [23,21,65,43,64,6]
+
+for(let i=0; i<myData2.length; i++){
+    for(let j=0; j<myData2.length; j++){
+       if(myData2[j]<myData2[j+1]){
+        let temp = myData2[j]
+        myData2[j] = myData2[j+1]
+        myData2[j+1] = temp 
+       }
+    }
+}
+console.log(myData2)
