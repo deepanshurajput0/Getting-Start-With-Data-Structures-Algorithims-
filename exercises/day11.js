c73614eb9d6aeb112c1400524b7321dbd1b8a609
deepanshu verma <deepanshuverma186@gmail.com>
2025-02-04 05:25:45 -0800

    Find Common elements between 2 arrays

--- exercises/day11.js
+++ exercises/day11.js
@@ -2,17 +2,63 @@
 // How to remove duplicates from an array
 
 function removeDuplicates(arr){
-    let uniqueArray =[]
-    for(let i=0; i<arr.length; i++){
-       if(!uniqueArray.includes(arr[i])){
-         uniqueArray[uniqueArray.length] = arr[i]
+   const unique = []
+   for(let i=0; i<arr.length; i++){
+       if(!unique.includes(arr[i])){
+        unique[unique.length] = arr[i]
        }
-    }
-    return uniqueArray
+   }
+   return unique
 }
 
-const result =  removeDuplicates([1,2,2,3,2,4,5,3,5,4])
+let result0 = removeDuplicates([1,2,2,1,3,4,2,4,5,23,4,23])
+console.log(result0)
+
+
+// const myarray = [12, 32, 34, 23, 65];
+// const myarray2 = [23,21,12,34,32,1]
+
+function commonValues(arr1,arr2){
+    let common = []
+    for(let i=0; i<arr2.length; i++){
+       if(arr1.includes(arr2[i])){
+           common[common.length] = arr2[i] 
+       }
+    }
+    return common
+}
+
+let result2 = commonValues(myarray,myarray2)
+console.log(result2)
+
+
+
+
+/// Find Common Elements Between two Arrays without uisng inbuild methods 
+
+const myarray = [12, 32, 34, 23, 65];
+const myarray2 = [23, 21, 12, 34, 32, 1];
+
+function commonValues(arr1,arr2){
+   const common =[]
+   for(let i=0; i<arr1.length; i++){
+      for(let j=0; j<arr2.length; j++){
+         if(arr1[i] === arr2[j]){
+             let alreadyexists = false
+             for(let k=0; k<common.length; k++){
+               if(common[k] === arr1[i]){
+                alreadyexists = true;
+                break
+               }
+             }
+             if(!alreadyexists){
+                common[common.length] = arr1[i]
+             }
+         }
+      }
+   }
+   return common
+}
+
+let result = commonValues(myarray,myarray2)
 console.log(result)
-
-
-
--- exercises/tempCodeRunnerFile.js
+++ exercises/tempCodeRunnerFile.js
@@ -1,2 +1,12 @@
+function removeDuplicates(arr){
+//    const unique = []
+//    for(let i=0; i<arr.length; i++){
+//        if(!unique.includes(arr[i])){
+//         unique[unique.length] = arr[i]
+//        }
+//    }
+//    return unique
+// }
 
-   return result
+// let result = removeDuplicates([1,2,2,1,3,4,2,4,5,23,4,23])
+// console.log(result)
