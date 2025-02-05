4642835c5fb8990d727779a412fac7be2aefc9eb
deepanshu verma <deepanshuverma186@gmail.com>
2025-02-04 16:52:29 -0800

    find Uniques without using inbuild methods

--- exercises/day11.js
+++ exercises/day11.js
@@ -18,47 +18,81 @@ console.log(result0)
 // const myarray = [12, 32, 34, 23, 65];
 // const myarray2 = [23,21,12,34,32,1]
 
-function commonValues(arr1,arr2){
-    let common = []
-    for(let i=0; i<arr2.length; i++){
-       if(arr1.includes(arr2[i])){
-           common[common.length] = arr2[i] 
-       }
-    }
-    return common
-}
+// function commonValues(arr1,arr2){
+//     let common = []
+//     for(let i=0; i<arr2.length; i++){
+//        if(arr1.includes(arr2[i])){
+//            common[common.length] = arr2[i] 
+//        }
+//     }
+//     return common
+// }
 
-let result2 = commonValues(myarray,myarray2)
-console.log(result2)
+// let result2 = commonValues(myarray,myarray2)
+// console.log(result2)
 
 
 
 
 /// Find Common Elements Between two Arrays without uisng inbuild methods 
 
-const myarray = [12, 32, 34, 23, 65];
-const myarray2 = [23, 21, 12, 34, 32, 1];
 
-function commonValues(arr1,arr2){
-   const common =[]
-   for(let i=0; i<arr1.length; i++){
-      for(let j=0; j<arr2.length; j++){
-         if(arr1[i] === arr2[j]){
-             let alreadyexists = false
-             for(let k=0; k<common.length; k++){
-               if(common[k] === arr1[i]){
-                alreadyexists = true;
-                break
-               }
-             }
-             if(!alreadyexists){
-                common[common.length] = arr1[i]
-             }
-         }
-      }
-   }
-   return common
+
+const myarray = [12, 32, 34, 23, 65,34];
+const myarray2 = [23,21,12,34,32,1]
+
+function findCommon(arr1,arr2){
+   let common = []
+    for(let i=0; i<arr1.length; i++){
+       for(let j=0; j<arr2.length; j++){
+          if(arr1[i]=== arr2[j]){
+            let alreadyexists = false
+            for(let k=0; k<common.length; k++){
+               if(common[k]===arr1[i]){
+                  alreadyexists = true
+                  break;
+               }  
+            }
+            if(!alreadyexists){
+               common[common.length] = arr1[i]
+            }
+          }
+       }
+    }
+    return common
 }
 
-let result = commonValues(myarray,myarray2)
+
+let result = findCommon(myarray,myarray2)
 console.log(result)
+
+
+
+
+
+const newArray =[1,2,2,4,12,32,231,43,32,12]
+
+
+function findUniques(newArray){
+   let uniques =  []
+   for(let i=0; i<newArray.length; i++){
+      let duplicates = false
+      for(let j=0; j<uniques.length; j++){
+          if(newArray[i]===uniques[j]){
+               duplicates = true  
+               break; 
+          }
+      }
+      if(!duplicates){
+         uniques[uniques.length] = newArray[i]
+      }
+   }
+   return uniques
+}
+
+let result4 = findUniques(newArray)
+console.log(result4)
+
+
+
+
