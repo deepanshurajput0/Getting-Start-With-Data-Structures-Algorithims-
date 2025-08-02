9bb5e7acb4260fa51b610b37fedf2665ecd78087
deepanshu verma <deepanshuverma186@gmail.com>
2025-08-02 22:50:04 +0530

    basic async concepts in javascript

--- revision/day4.js
+++ revision/day4.js
@@ -1,1 +1,33 @@
 //// asynchronous javascript
+
+// function findSum(n){
+//     let a = 0;
+//     for(let i=0; i<n; i++){
+//       a++      
+//     }
+//     return a
+// }
+
+
+// function findSumIn2Sec(){
+//    console.log(findSum(100000))
+// } 
+
+function sleepSync(){
+    let b = 0;
+    for(let i=0; i<100000000; i++){
+       b = i+1; 
+    }
+    // console.log(b)
+    return b
+
+}
+
+
+sleepSync();
+
+// setTimeout(findSumIn2Sec,3000)
+
+console.log("hi there")
+
+
